b99c3a181a29647bc0855bf94662df18820bc9c5
YashwantHabib <yashwanthabib567@gmail.com>
2024-11-04 23:12:10 +0530

    Added gevent for render

---
+++ test.py
@@ -0,0 +1,20 @@
+import requests
+
+# URL of the local Flask server
+url = "http://127.0.0.1:5000/predict"
+
+# Open the image file in binary mode
+with open(r"img.JPG", "rb") as image_file:
+    # Prepare the files dictionary for the POST request
+    files = {"file": image_file}
+    
+    # Send the POST request to the server
+    response = requests.post(url, files=files)
+    
+    # Check the response
+    if response.status_code == 200:
+        prediction = response.json()
+        print("Predicted Label:", prediction["label"])
+        print("Confidence:", prediction["confidence"])
+    else:
+        print("Error:", response.json())
